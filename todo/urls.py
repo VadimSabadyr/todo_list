94cc2cc49bde277117468a5ba75f628a04f551f1
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-14 11:28:05 +0200

    create TaskCreate & TaskDeleteView in view

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import TaskView, TagView
+from .views import TaskView, TagView, TaskCreateView, TaskDeleteView
 
 app_name = "todo"
 
 urlpatterns = [
     path("", TaskView.as_view(), name="task_list"),
+    path("create/", TaskCreateView.as_view(), name="task_create"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
+
     path("tags/", TagView.as_view(), name="tag_list"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task, Tag
@@ -7,6 +8,17 @@ from todo.models import Task, Tag
 class TaskView(generic.ListView):
     model = Task
     paginate_by = 5
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task_list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task_list")
 
 
 class TagView(generic.ListView):
