07c37c068b8d308c1a94b47138dd0b1ed583f9b0
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-11-14 11:04:35 +0200

    create ToDoListView in view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+class ToDoListView(generic.ListView):
+    model = Task
+    context_object_name = "todo_list"
+    template_name = "todo/todo_list.html"
+    paginate_by = 5
